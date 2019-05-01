f84273cd9e3341a7af89ba9c7ff7a15189e39bc7
Antonio <aolmolopez97@gmail.com>
2019-05-01 20:13:24 +0200

    Subida

--- __manifest__.py
+++ __manifest__.py
@@ -2,9 +2,9 @@
 {
     'name': "CDPELOTAS3763Y",
 
-    'summary': "Modulo de para tarea Online 4 de Sistemas de Gestión Empresarial",
+    'summary': """Modulo de para tarea Online 4 de Sistemas de Gestión Empresarial""",
 
-    'description': "Modulo de para tarea Online 4 de Sistemas de Gestión Empresarial",
+    'description': """Modulo de para tarea Online 4 de Sistemas de Gestión Empresarial""",
 
     'author': "Antonio Olmo López",
     'website': "www.iestrassierra.conm",
@@ -12,7 +12,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Gestión de Deportes',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -22,6 +22,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/socios.xml',
+        'views/instalaciones.xml'
     ],
     # # only loaded in demonstration mode
     # 'demo': [
