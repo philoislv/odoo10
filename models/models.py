8c6dba436798014a13d6b537a95f0b91257e17bd
Antonio <aolmolopez97@gmail.com>
2019-05-01 20:09:31 +0200

    Subida

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+#Modelo Socio
 class Socio(models.Model):
  _name = 'cdpelotas3763_y.socios'
 
@@ -9,3 +10,15 @@ class Socio(models.Model):
  direccion = fields.Char(string="Direccion")
  telefono = fields.Integer(string="Teléfono")
  fecha_alta = fields.Date(string="Fecha de Alta")
+ #Campo reservas
+ #Modelo Instalaciones
+class Instalaciones(models.Model):
+ _name = 'cdpelotas3763_y.instalaciones'
+ num_pista = fields.Integer(string="Numero de pista")
+ nombre_pista = fields.Char(string="Nombre de Pista")
+ superficie = fields.Selection([('cemento','Cemento'),('hierba','Hierba'),('moqueta','Moqueta'),('tierra','Tierra')])
+ luz = fields.Selection([('si','Si'),('no','No')])
+ precio = fields.Integer(string="Precio/hora de la pista")
+ estado = fields.Selection([('disponible','Disponible'),('mantenimiento','Mantenimiento')])
+ #Campo deporte
+ #Campo reservas
